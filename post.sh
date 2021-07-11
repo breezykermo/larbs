6e091acd7c7732fa1ead19fd612aeccbd6d0e3af
Lachie Kermode <lachiekermode@gmail.com>
2021-07-11 09:24:01 +0200

    update post

--- post.sh
+++ post.sh
@@ -5,3 +5,8 @@ git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 rm -rf ~/.doom.d
 git clone git@github.com:breezykermo/.doom.d ~/.doom.d
 ~/.emacs.d/bin/doom sync
+
+# NVM, node and coc
+fisher install jorgebucaran/nvm.fish
+nvm install latest
+cd ~/.config/nvim/plugged/coc.nvim && npm i
